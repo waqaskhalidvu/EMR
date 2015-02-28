01999b18ad9087107e2f79a88be3633019153b25
Fahad Ali <bc110201815@vu.edu.pk>
2015-03-01 00:24:27 +0500

    Pininterest

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -9,7 +9,7 @@
                             <li><a class="fa fa-twitter" href="https://twitter.com/vu_emr"></a></li>
                             <li><a class="fa fa-facebook" href="https://www.facebook.com/vuemr"></a></li>
                             <li><a class="fa fa-google-plus" href="https://plus.google.com/104453698044276635098/about"></a></li>
-                            <li><a class="fa fa-pinterest" href="#"></a></li>
+                            <li><a class="fa fa-pinterest" href="https://www.pinterest.com/vuemr/"></a></li>
                         </ul>
                     </div>
                 </div>
