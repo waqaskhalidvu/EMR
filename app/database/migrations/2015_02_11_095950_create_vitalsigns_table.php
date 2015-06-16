843d80ad86b5280fe6403ffdfd21ba6cf8576328
Fahad Ali <fahad@softpyramid.com>
2015-06-17 00:15:12 +0500

    minor

--- app/database/migrations/2015_02_11_095950_create_vitalsigns_table.php
+++ app/database/migrations/2015_02_11_095950_create_vitalsigns_table.php
@@ -16,20 +16,13 @@ class CreateVitalsignsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('weight');
-			$table->string('weight_unit');
 			$table->string('height');
-			$table->string('height_unit');
 			$table->integer('bp_systolic');
-			$table->string('bp_systolic_unit');
 			$table->integer('bp_diastolic');
-			$table->string('bp_diastolic_unit');
 			$table->string('blood_group');
 			$table->string('pulse_rate');
-			$table->string('pulse_rate_unit');
 			$table->string('respiration_rate');
-			$table->string('respiration_rate_unit');
 			$table->integer('temprature');
-			$table->string('temprature_unit');
 			$table->text('note');
 			$table->string('patient_id');
             $table->string('appointment_id');
